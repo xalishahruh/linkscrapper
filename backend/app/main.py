af58860a2e5b58f7ea3d3b40cf71343f973be60e
khali, shaxrux <satelnik32@gmail.com>
2026-01-24 14:19:22 +0500

    update main.py to initiate db

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
 from backend.app.api.analyze import router as analyze_router
+from backend.app.db import Base, engine
+from backend.app.models import db_models  # IMPORTANT: registers models
+
 app = FastAPI(
     title="Link Scrapper API",
     description="Backend API to analyze URLS and detect potential threats",
     version="1.0",
 
 )
+Base.metadata.create_all(bind=engine)
+
 app.include_router(analyze_router)
 @app.get("/")
 def root():
